913daf4de1ab50a5146399894318d22ec18ff3a2
whoovian <62331633+whoovian@users.noreply.github.com>
2020-04-16 06:37:36 +0300

    Profiilin tulostusta part miljoona

--- profiili.php
+++ profiili.php
@@ -4,10 +4,13 @@
 ?>
 
 <?php
+
 //kirjautuneen käyttäjän tietoja 
 //https://www.php.net/manual/en/function.print-r.php
 //mysql_fetch_assoc doesm't print out a numeric index (0, 1, 2, ..) +  associative key.
 //https://stackoverflow.com/questions/2970936/how-to-echo-out-table-rows-from-the-db-php
+//https://stackoverflow.com/questions/27431601/how-to-echo-the-row-into-my-html
+
 $query = "SELECT * FROM users WHERE id = " . $_SESSION["id"]; 
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
 while($row = mysqli_fetch_assoc($result)) { ?>
@@ -21,15 +24,18 @@ while($row = mysqli_fetch_assoc($result)) { ?>
 <body>
     <div clas="box">
         <div class=" tm-text-color-white nav-link" >
-            <h2><?php echo $row["Etunimi"]; ?></h2>
-            <h2><?php echo $row["Sukunimi"]; ?></h2>
-            <h2><?php echo $row["Sukupuoli"]; ?></h2>
-            <h2><?php echo $row["ika"]; ?></h2>
-            <h2><?php echo $row["sahkoposti"]; ?></h2>
-            <h2><?php echo $row["paino"]; ?></h2>
-            <h2><?php echo $row["pituus"]; ?></h2>
-            <h2><?php echo $row["makssyke"]; ?></h2>
-            <h2><?php echo $row["leposyke"]; ?></h2>
+            <h2>Käyttäjän <?php echo $row["username"]; ?> tiedot</h2>
+            <h3>Etunimi: <?php echo $row["Etunimi"]; ?></h3>
+            <h3>Sukunimi: <?php echo $row["Sukunimi"]; ?></h3>
+            <h3>Sukupuoli: <?php echo $row["Sukupuoli"]; ?></h3>
+            <h3>Ikä: <?php echo $row["ika"]; ?></h3>
+            <h3>Sähköpostiosoite: <br> 
+            <br>
+            <?php echo $row["sahkoposti"]; ?></h3>
+            <h3>Paino: <?php echo $row["paino"]; ?></h3>
+            <h3>Pituus: <?php echo $row["pituus"]; ?></h3>
+            <h3>Maksimisyke <?php echo $row["makssyke"]; ?></h3>
+            <h3>Leposyke <?php echo $row["leposyke"]; ?></h3>
         </div>
         <a class=" tm-text-color-white nav-link" href="insert.php">Asetukset</a>
         <a class=" tm-text-color-white nav-link" href="etusivu.php">Etusivu</a>
